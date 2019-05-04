7522d1caa3ee08108d56ed1984baaac2db46ed38
alejandrobarrios <barriosalejandro792@gmail.com>
2019-05-04 03:08:40 -0300

    includes get query in App.java

--- trivia/App.java
+++ trivia/App.java
@@ -6,6 +6,7 @@ import static spark.Spark.get;
 import static spark.Spark.post;
 
 import java.util.Map;
+import java.util.*;
 
 import org.javalite.activejdbc.Base;
 import org.javalite.activejdbc.DB;
@@ -39,6 +40,9 @@ public class App
         User user = new User();
         user.set("username", bodyParams.get("username"));
         user.set("password", bodyParams.get("password"));
+        user.set("name", bodyParams.get("name"));
+        user.set("lastname", bodyParams.get("lastname"));
+        user.set("dni", bodyParams.get("dni"));
         user.saveIt();
 
         res.type("application/json");
@@ -50,7 +54,6 @@ public class App
         Map<String, Object> bodyParams = new Gson().fromJson(req.body(), Map.class);
 
         Question question = new Question();
-        question.set("id",bodyParams.get("id"));
         question.set("category", bodyParams.get("category"));
         question.set("description", bodyParams.get("description"));
         question.saveIt();
@@ -64,7 +67,6 @@ public class App
         Map<String, Object> bodyParams = new Gson().fromJson(req.body(), Map.class);
 
         Option option = new Option();
-        option.set("id",bodyParams.get("id"));
         option.set("category", bodyParams.get("category"));
         option.set("description", bodyParams.get("description"));
         option.saveIt();
@@ -74,22 +76,49 @@ public class App
         return option.toJson(true);
       });
 
-      get("/users/:name", (req, res) -> {//consulta dado un nombre
-        LazyList<User> user = User.where("username = ?",req.params(":name"));
-        User usuario = user.get(0);
-        return usuario.get("username");
+      get("/users", (req, res) -> {//get all users load
+        LazyList<User> user = User.findAll();
+        List<String> lista = new ArrayList<String>();
+        for(User u: user ){
+          String usuario = "Su username es: " + u.get("username") + ", su dni es: " + u.get("dni");
+          lista.add(usuario);
+        }
+        return lista;
+      });
+
+      get("/questions", (req, res) -> {//get all questions load
+        LazyList<Question> question = Question.findAll();
+        List<String> lista = new ArrayList<String>();
+        for(Question q: question ){
+          String pregunta = "Su categoria es: " + q.get("category") + ", su descripcion es: " + q.get("description");
+          lista.add(pregunta);
+        }
+        return lista;
+      });    
+
+      get("/options/:id", (req, res) -> {//get all options of a given question_id
+        LazyList<Option> option = Option.where("question_id = ?",req.params(":id"));
+        List<String> lista = new ArrayList<String>();
+        for(Option u: option ){
+          String opcion = "Su categoria es: " + u.get("category") + ", su descripcion es: " + u.get("description");
+          lista.add(opcion);
+        }
+        return lista;
+      });  
+
+      get("/users/:id", (req, res) -> {//verfication that a user is load
+        LazyList<User> user = User.where("id = ?", req.params(":id"));
+        return user.get(0);
       });
       
-      get("/questions/:id", (req, res) -> {//consulta dada una categoria
-        LazyList<Question> question = Question.where("id = ?",req.params(":id"));
-        Question query = question.get(0);
-        return "la pregunta pertenece a " + query.get("category") + " y es la siguiente: " + query.get("description");
+      get("/questions/:id", (req, res) -> {//verification if a question is load
+        Question question = Question.findFirst("id = ?",req.params(":id"));
+        return "La pregunta pertenece a " + question.get("category") + " y es la siguiente: " + question.get("description");
       });
       
-      get("/questions/:category", (req, res) -> {//consulta dada una categoria
-        LazyList<Option> option = Option.where("category = ?",req.params(":category"));
-        Option answer = option.get(0);
-        return answer.get("description");
+      get("/questions/:id", (req, res) -> {//verification of a option i load
+        Option option = Option.findFirst("question_id = ?",req.params(":id"));
+        return option.get("description");
       });
       
 
