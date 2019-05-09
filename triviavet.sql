26e9797e338a631839b6edf7851d246362b22194
maxigaspero <maxigaspero@gmail.com>
2019-05-09 15:22:46 -0300

    Update sql file

--- triviavet.sql
+++ triviavet.sql
@@ -4,8 +4,8 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users(
     id integer auto_increment primary key,
     username varchar(20),
-    pass varchar(20),
-    nam varchar(20),
+    password varchar(20),
+    name varchar(20),
     lastname varchar(20),
     dni integer,
     admin bool
@@ -14,12 +14,14 @@ DROP TABLE IF EXISTS questions;
 CREATE TABLE IF NOT EXISTS questions(
     id integer auto_increment primary key,
 	category varchar(30),
-    description varchar(100)
+    description varchar(100),
+    see bool
+    
 );
 DROP TABLE IF EXISTS options;
 CREATE TABLE IF NOT EXISTS options(
     id integer auto_increment primary key,
     description varchar(100),
     correct bool,
-    question_id int
-);
+    question_id int -- ON DELETE CASCADE
+);	
