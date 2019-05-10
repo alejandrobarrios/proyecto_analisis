93d0ecbc06844a05e7961b259db9130bb261e178
maxigaspero <maxigaspero@gmail.com>
2019-05-10 11:21:37 -0300

    Update SQL File- Add Restrictions

--- src/main/resources/triviavet.sql
+++ src/main/resources/triviavet.sql
@@ -8,20 +8,28 @@ CREATE TABLE IF NOT EXISTS users(
     name varchar(20),
     lastname varchar(20),
     dni integer,
-    admin bool
+    admin bool,
+    created_at DATETIME,
+	updated_at DATETIME
 );
+
 DROP TABLE IF EXISTS questions;
 CREATE TABLE IF NOT EXISTS questions(
     id integer auto_increment primary key,
 	category varchar(30),
     description varchar(100),
-    see bool
-    
+    see bool,
+    created_at DATETIME,
+	updated_at DATETIME
 );
+
 DROP TABLE IF EXISTS options;
 CREATE TABLE IF NOT EXISTS options(
     id integer auto_increment primary key,
     description varchar(100),
     correct bool,
-    question_id int -- ON DELETE CASCADE
+    question_id int,
+    foreign key(question_id) references questions(id) ON DELETE CASCADE,
+    created_at DATETIME,
+	updated_at DATETIME
 );	
