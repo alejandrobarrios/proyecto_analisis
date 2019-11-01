6d7edf8031659ca252d0aba7b894010bf3eed246
alejandrobarrios <barriosalejandro792@gmail.com>
2019-11-01 11:22:06 -0300

    add new components

---
+++ admin/src/statQuiru.js
@@ -0,0 +1,58 @@
+import React, {Component } from 'react';
+import { Button, DropdownButton, Dropdown } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import "./App.css";
+
+
+export default class statQuiru extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      category:'',
+
+    };
+  }
+
+
+
+  componentDidMount() {
+
+
+       //fetch('http://jsonplaceholder.typicode.com/users')
+       fetch('http://localhost:4567/admin/statCat',{
+        method: 'POST',
+        body: '{"category":"examen_clinica"}'
+      })
+        .then(response => response.json())
+        .then((data) => {
+          this.setState({ contacts : data })
+          console.log(this.state.contacts)
+        })
+        .catch(console.log)
+
+    }
+
+
+
+  onSubmit =(event) =>{
+
+    //para no refrescar el formulario cada vez que presiono el boton
+    console.log(this.state);
+    event.preventDefault();
+  }
+
+
+
+  render() {
+    return (
+      <div>
+
+        <center><h1>Estadísticas</h1></center>
+        <h3>¿Qué desea hacer?</h3>
+
+
+
+      </div>
+    );
+  }
+}
