01b94f2c909ec4f09f53cfa1d3cb388487f3854a
maxigaspero <maxigaspero@gmail.com>
2019-11-03 22:57:41 -0300

    Modify css

--- admin/src/home.js
+++ admin/src/home.js
@@ -2,6 +2,7 @@ import React, {Component } from 'react';
 import { Button, DropdownButton, Dropdown, ButtonGroup } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import "./App.css";
+import logo from './trviavetlogo.png';
 //import { createSwitchNavigator } from "@react-navigation/core";
 //import { createBrowserApp } from "@react-navigation/web";
 
@@ -30,29 +31,35 @@ export default class home extends Component {
   //const App = createBrowserApp(MyNavigator);
   render() {
     return (
-      <div className= "App-header">
-
-        <center><h1>Welcome to TriviaVet</h1></center>
-        <h3>¿Qué desea hacer?</h3>
-        <div className="menu">
-          <ButtonGroup>
-            <DropdownButton id="dropdown-basic-button " variant="info" title="Preguntas">
-              <Dropdown.Item href="/loadQuestion">Cargar una pregunta</Dropdown.Item>
-              <Dropdown.Item href="/modifyQues">Modificar una pregunta</Dropdown.Item>
-              <Dropdown.Item href="/deleteQues">Borrar una pregunta</Dropdown.Item>
-            </DropdownButton>
-            <DropdownButton id="dropdown-basic-button " variant="info" title="Estadisticas">
-              <Dropdown.Item href="/exClin">Examen Clinica</Dropdown.Item>
-              <Dropdown.Item href="/farma">Farmacologia</Dropdown.Item>
-              <Dropdown.Item href="/edpide">Epidemiologia</Dropdown.Item>
-              <Dropdown.Item href="/enferm">Enfermedades</Dropdown.Item>
-              <Dropdown.Item href="/quiru">Quirurgica</Dropdown.Item>
-              <Dropdown.Item href="/clinMed">Clinica Medica</Dropdown.Item>
-            </DropdownButton>
-            <Button href="/privilegesAdmin" variant="success" type="submit">Dar Privilegios</Button>
-
-            <Button href="/" variant="danger" type="submit">Cerrar Sesión</Button>
-          </ButtonGroup>
+        <div className= "App-header">
+          <img className="begin-logo" src={logo} alt="logo" />
+          
+          <div className= "App-header2">
+          <center><h1>Bienvenido a TriviaVet Admin</h1></center>
+          <h3>¿Qué desea hacer?</h3>
+          <div className="menu">
+            <ButtonGroup>
+              <DropdownButton id="dropdown-basic-button " variant="info" title="Preguntas">
+                <Dropdown.Item href="/loadQuestion">Cargar una pregunta</Dropdown.Item>
+                <Dropdown.Item href="/modifyQues">Modificar una pregunta</Dropdown.Item>
+                <Dropdown.Item href="/deleteQues">Borrar una pregunta</Dropdown.Item>
+              </DropdownButton>
+              <DropdownButton id="dropdown-basic-button " variant="info" title="Estadisticas">
+                <Dropdown.Item href="/exClin">Examen Clínica</Dropdown.Item>
+                <Dropdown.Item href="/farma">Farmacología</Dropdown.Item>
+                <Dropdown.Item href="/edpide">Epidemiología</Dropdown.Item>
+                <Dropdown.Item href="/enferm">Enfermedades</Dropdown.Item>
+                <Dropdown.Item href="/quiru">Quirúrgica</Dropdown.Item>
+                <Dropdown.Item href="/clinMed">Clínica Médica</Dropdown.Item>
+              </DropdownButton>
+              <div className="menu">
+                <Button href="/privilegesAdmin" variant="info" type="submit">Dar Privilegios</Button>
+              </div>
+              <div className="menu">
+                <Button href="/" variant="info" type="submit">Cerrar Sesión</Button>
+              </div>
+            </ButtonGroup>
+          </div>
         </div>
     </div>
     );
